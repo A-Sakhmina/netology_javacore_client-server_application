672d594b5524ceb1cab9f754b974c7c8fb954b29
Sakhmina <asadullaevasakhmina@gmail.com>
2022-03-29 19:40:25 +0300

    Changed name in Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,22 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) {
         //localhost
-        String host = "127.0.0.1";
+        String host = "localhost";
         int port = 8080;
 
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("GET / HTTP/1.1\n" + "Host: netology.ru\n\n\n");
+            out.println("Sakhmina");
             String resp = in.readLine();
             System.out.println(resp);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
